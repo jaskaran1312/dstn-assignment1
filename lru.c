93079199b7bc656d91fce4e78299ed6a84c47547
visargsoneji <visargbs22@gmail.com>
2021-04-16 22:20:59 +0530

    implemented MM LRU functions

---
+++ lru.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "hardware.h"
+#include "lru.h"
+
+int getMMLRU(struct Hardware *hardware) {
+    int frameNumber;
+    int minLRU=__INT_MAX__;
+    for(int i=0;i<65536;i++) {
+        if(hardware->mainMemory->lru[i] <= minLRU){
+            minLRU = hardware->mainMemory->lru[i];
+            frameNumber = i;
+        }
+    }
+    return frameNumber;
+}
+
+void shiftMMLRU(struct Hardware *hardware) {
+    for(int i=0;i<65536;i++) {
+        hardware->mainMemory->lru[i] = hardware->mainMemory->lru[i] >> 1;
+    }
+    return;
+}
+
+void updateMMLRU(struct Hardware *hardware, int frameNumber) {
+    hardware->mainMemory->lru[frameNumber] = hardware->mainMemory->lru[frameNumber] | 0x8000;
+    return;
+}

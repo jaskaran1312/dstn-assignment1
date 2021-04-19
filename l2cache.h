50779fe27f3d3780bfa2f6f9dd9262744c190595
visargsoneji <visargbs22@gmail.com>
2021-04-19 17:43:16 +0530

    modified updateL2 and implemented writeBackL2

--- l2cache.h
+++ l2cache.h
@@ -1,2 +1,3 @@
 int fetchL2Cache (int64_t pa, struct Hardware *hardware);
-void updateL2Cache(int64_t pa, struct Hardware *hardware);
+int updateL2Cache(int64_t pa, struct Hardware *hardware);
+void writeBackL2(int64_t pa, struct Hardware *hardware);
